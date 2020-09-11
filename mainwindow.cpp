bf19459583f3abee21e1022de267b4f60caa75d5
Donald Rudquist <donaldrudquist@gmail.com>
2020-09-11 09:36:20 -0500

    Setup skeleton defintions of functions for classes 'MainWindow' and 'ScribbleArea'

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,81 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "scribblearea.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    scribbleArea = new ScribbleArea();
+    setCentralWidget(scribbleArea);
+
+    createActions();
+    createMenus();
+
+    setWindowTitle("Scribble");
+    resize(500, 500);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+
+}
+
+void MainWindow::createActions()
+{
+
+}
+
+void MainWindow::createMenus()
+{
+
+}
+
+bool MainWindow::maybeSave()
+{
+
+}
+
+bool MainWindow::saveFile(const QByteArray &fileFormat)
+{
+
+}
+
+void MainWindow::open()
+{
+
+}
+
+void MainWindow::save()
+{
+
+}
+
+void MainWindow::color()
+{
+
+}
+
+void MainWindow::penColor()
+{
+
+}
+
+void MainWindow::penWidth()
+{
+
+}
+
+void MainWindow::about()
+{
+
 }
 
--- scribblearea.cpp
+++ scribblearea.cpp
@@ -4,3 +4,68 @@ ScribbleArea::ScribbleArea(QWidget *parent) : QWidget(parent)
 {
 
 }
+
+void ScribbleArea::setPenColor(const QColor &newColor)
+{
+
+}
+
+void ScribbleArea::setPenWidth(int newWidth)
+{
+
+}
+
+bool ScribbleArea::openImage(const QString &fileName)
+{
+
+}
+
+bool ScribbleArea::saveImage(const QString &fileName, const char *fileFormat)
+{
+
+}
+
+void ScribbleArea::clearImage()
+{
+
+}
+
+void ScribbleArea::printImage()
+{
+
+}
+
+void ScribbleArea::mousePressEvent(QMouseEvent *event)
+{
+
+}
+
+void ScribbleArea::mouseMoveEvent(QMouseEvent *event)
+{
+
+}
+
+void ScribbleArea::mouseReleaseEvent(QMouseEvent *event)
+{
+
+}
+
+void ScribbleArea::paintEvent(QPaintEvent *event)
+{
+
+}
+
+void ScribbleArea::resizeEvent(QResizeEvent *event)
+{
+
+}
+
+void ScribbleArea::drawLineTo(const QPoint &endPoint)
+{
+
+}
+
+void ScribbleArea::resizeImage(QImage *image, const QSize &newSize)
+{
+
+}
