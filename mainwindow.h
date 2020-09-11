8883ca4b7dd6dad835e45a8f82a182b4e8cdd6b2
Donald Rudquist <donaldrudquist@gmail.com>
2020-09-11 08:41:30 -0500

    Declared functions in mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QList>
+
+class ScribbleArea;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +18,45 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+
+    void closeEvent(QCloseEvent* event) override;
+
 private:
     Ui::MainWindow *ui;
+
+    ScribbleArea* scribbleArea;
+
+    //menus
+    QMenu* saveAsMenu;
+    QMenu* fileMenu;
+    QMenu* openMenu;
+    QMenu* helpMenu;
+
+    //actions
+    QAction* openAct;
+    QAction* penColorAct;
+    QAction* penWidthAct;
+    QAction* printAct;
+    QAction* clearScreenAct;
+    QAction* aboutAct;
+    QAction* aboutQtAct;
+
+    QList<QAction*> saveAsActions;
+
+    void createActions();
+    void createMenus();
+
+    bool maybeSave();
+    bool saveFile(const QByteArray& fileFormat);
+
+private slots:
+
+    void open();
+    void save();
+    void color();
+    void penColor();
+    void penWidth();
+    void about();
 };
 #endif // MAINWINDOW_H
